c28a89a31505a1d5d9ce5a01ffd9c27531c32e36
ADMIN <ADMIN@LAB1-MAY20>
2024-03-04 08:43:43 +0700

    Add project files.

---
+++ LTCB_VONGLAP While/LTCB_VONGLAP While.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello World!\n";
+	int i=0,so;
+	cout << "Nhap so:";
+	cin >> so;
+	while (i <= so)
+	{
+		cout << i << " ";
+		i++;
+	}
+	return 0;
+}
